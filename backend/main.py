28ac6131434a05d155759b47ba5febc14f6ba3b5
Alex Calfucoy <alexcalfucoy2@gmail.com>
2026-09-01 22:11:05 -0300

    actualizo proyecto

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+# ======= API =======
+# Creamos nuestra API usando FastAPI
 app = FastAPI()
 
+#Frontend: parte que ve y usa el usuario
+# Permite que el frontend se conecte con la API
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -10,20 +14,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Lista donde guardamos los usuarios
 usuarios = []
 
-
+# GET: obtener usuarios
 @app.get("/usuarios")
 def obtener_usuarios():
     return usuarios
 
-
+# POST: crear usuario
 @app.post("/usuarios")
 def crear_usuario(usuario: dict):
     usuarios.append(usuario)
     return usuario
 
-
+# DELETE: eliminar usuario
 @app.delete("/usuarios/{indice}")
 def eliminar_usuario(indice: int):
     usuarios.pop(indice)
